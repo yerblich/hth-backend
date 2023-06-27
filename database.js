7a7151b3b1e691e1cb277466626acbdb48a454ed
DeFragMentor01 <93282561+DeFragMentor01@users.noreply.github.com>
2023-06-27 22:38:10 +0300

    Add files via upload

--- database.js
+++ database.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'itribes_database',
-  password: 'Rabeinu18!',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'itribes_database',
+//   password: 'Rabeinu18!',
+//   port: 5432,
+// });
 
 module.exports = pool;
