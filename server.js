9d28fb5acf25b6b361bba156dac3a24b8c3bbed4
DeFragMentor01 <93282561+DeFragMentor01@users.noreply.github.com>
2023-06-28 17:10:08 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const start = async () => {
     console.log('Connected to the database');
     client.release();
 
-    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
+    await fastify.listen('0.0.0.0');
     console.log('Server is running on port 3000');
   } catch (err) {
     console.error(err);
