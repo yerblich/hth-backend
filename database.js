fdbe6fddb5cadc161f8c72c347362aa28e1fa593
DeFragMentor01 <93282561+DeFragMentor01@users.noreply.github.com>
2023-06-28 17:17:59 +0300

    Add files via upload

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: '0.0.0.0',
   ssl: {
     rejectUnauthorized: false,
   }
