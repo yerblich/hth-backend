4d37e532d915ea46f407fff622ec00721b24d741
DeFragMentor01 <93282561+DeFragMentor01@users.noreply.github.com>
2023-06-28 17:55:46 +0300

    Add files via upload

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:Rabeinu18!@0.0.0.0:5432/itribes_database",
+  connectionString: "postgresql://postgres:Rabeinu18!@localhost:5432/itribes_database",
   ssl: {
     rejectUnauthorized: false,
   }
