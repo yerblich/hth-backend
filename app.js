fe7623d706a9ebf052c0d0e6d6d92d38156df327
DeFragMentor01 <93282561+DeFragMentor01@users.noreply.github.com>
2023-06-29 17:05:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,46 +12,6 @@ fastify.register(cors, {
   origin: 'http://localhost:3001',
 });
 
-fastify.get('/importUsers', async (req, reply) => {
-  const dataRows = [];
-  fs.createReadStream('/users.csv')
-    .pipe(csv())
-    .on('data', (data) => dataRows.push(data))
-    .on('end', () => {
-      dataRows.forEach(row => {
-        // Adjust this query to match your specific table and data structure
-        pool.query('INSERT INTO users (id, firstname, lastname, username, gender, dateofbirth, village, community, city, state, country, membertype, email, createdat, updatedat, password, hashed_password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)', 
-          [row.id, row.firstname, row.lastname, row.username, row.gender, row.dateofbirth, row.village, row.community, row.city, row.state, row.country, row.membertype, row.email, row.createdat, row.updatedat, row.password, row.hashed_password], 
-          (error, results) => {
-            if (error) {
-              throw error;
-            }
-          });
-      });
-      reply.send({status: 'Import completed'});
-    });
-});
-
-fastify.get('/importVillages', async (req, reply) => {
-  const dataRows = [];
-  fs.createReadStream('/villages.csv')
-    .pipe(csv())
-    .on('data', (data) => dataRows.push(data))
-    .on('end', () => {
-      dataRows.forEach(row => {
-        pool.query('INSERT INTO villages (id, no, province, district, village_name, latitude, longitude, area_square_meter, hectares, shape_length, population) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)', 
-          [row.id, row.no, row.province, row.district, row.village_name, row.latitude, row.longitude, row.area_square_meter, row.hectares, row.shape_length, row.population], 
-          (error, results) => {
-            if (error) {
-              throw error;
-            }
-          });
-      });
-      reply.send({status: 'Import completed'});
-    });
-});
-
-
 fastify.get('/villages-info', async (request, reply) => {
   try {
     const client = await pool.connect();
