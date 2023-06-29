03f1c89e34509c020ac66b7dd1e55a77b94382b9
yerblich <yerblich@gmail.com>
2023-06-30 01:43:59 +0300

    db url

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgres://themainguy:qjaTCSBsraPKLNsyC7rnaYRJJGvcxyBE@dpg-cie7nft9aq0ce3bbjhgg-a/itribes_database_ur70",
+  connectionString:
+    "postgres://themainguy:qjaTCSBsraPKLNsyC7rnaYRJJGvcxyBE@dpg-cie7nft9aq0ce3bbjhgg-a.oregon-postgres.render.com/itribes_database_ur70",
   ssl: {
     rejectUnauthorized: false,
-  }
+  },
 });
 
 module.exports = pool;
